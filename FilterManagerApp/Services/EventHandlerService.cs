1f8575459dc3139b982e20414a928c2edd1abe2f
koolzoli ^ <grzegorz.sajewicz@gmail.com>
2023-05-30 10:16:52 +0200

    Cosmetic changes in EventHandlerService.

--- FilterManagerApp/Services/EventHandlerService.cs
+++ FilterManagerApp/Services/EventHandlerService.cs
@@ -1,18 +1,20 @@
-﻿using FilterManagerApp.Data.Entities;
+﻿using CsvHelper;
+using FilterManagerApp.Data.Entities;
 using FilterManagerApp.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FilterManagerApp.Services
 {
-    public class EventHandlerService: IEventHandlerService
+    public class EventHandlerService : IEventHandlerService
     {
         private readonly IFilterRepository _filterRepository;
 
-        public EventHandlerService(IFilterRepository filterRepository) 
+        public EventHandlerService(IFilterRepository filterRepository)
         {
             _filterRepository = filterRepository;
         }
@@ -30,6 +32,12 @@ namespace FilterManagerApp.Services
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine($"\nFilters saved successfully to files.\n");
             Console.ResetColor();
+
+            using (var writer = File.AppendText(@"NewFiles\filter-actions.txt"))
+            {
+                writer.WriteLine($"{DateTime.UtcNow} Files saved.");
+                writer.Dispose();
+            }
         }
 
         private void FilterRepositoryOnItemAdded(object? sender, Filter e)
@@ -37,6 +45,12 @@ namespace FilterManagerApp.Services
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine($"Filter {e.Name} added successfully.\n");
             Console.ResetColor();
+
+            using (var writer = File.AppendText(@"NewFiles\filter-actions.txt"))
+            {
+                writer.WriteLine($"{DateTime.UtcNow} Filter added, Id: {e.Id} Name: {e.Name}");
+                writer.Dispose();
+            }
         }
 
         private void FilterRepositoryOnItemRemoved(object? sender, Filter e)
@@ -44,6 +58,13 @@ namespace FilterManagerApp.Services
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine($"Filter {e.Name} removed successfully.\n");
             Console.ResetColor();
+
+            using (var writer = File.AppendText(@"NewFiles\filter-actions.txt"))
+            {
+                writer.WriteLine($"{DateTime.UtcNow} Filter removed, Id: {e.Id} Name: {e.Name}");
+                writer.Dispose();
+            }
+
         }
 
         private void FilterRepositoryOnItemChanged(object? sender, Filter e)
@@ -51,8 +72,18 @@ namespace FilterManagerApp.Services
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine($"Filter {e.Name} updated successfully.\n");
             Console.ResetColor();
+
+            using (var writer = File.AppendText(@"NewFiles\filter-actions.txt"))
+            {
+                writer.WriteLine($"{DateTime.UtcNow} Filter updated: {e.Id} {e.Name}");
+                writer.Dispose();
+            }
         }
 
     }
+
+ 
+
 }
+
  
